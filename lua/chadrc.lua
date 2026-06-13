27a20a2b2097fe92a8acd11295343c738b9d4997
Mooling0602 <clemooling@outlook.com>
2026-06-13 10:39:44 +0800

    Initial NvChad configuration

---
+++ lua/chadrc.lua
@@ -0,0 +1,35 @@
+local M = {}
+M.base46 = {
+  transparency = true,
+  hl_override = {
+    -- Solid background for file tree (transparency looks dark)
+    NvimTreeNormal = { bg = "black" },
+    NvimTreeNormalNC = { bg = "black" },
+    NvimTreeCursorLine = { bg = "black2" },
+    NvimTreeEndOfBuffer = { fg = "darker_black", bg = "black" },
+    NvimTreeWinSeparator = { fg = "darker_black", bg = "darker_black" },
+    -- Readable folder colors
+    NvimTreeFolderName = { fg = "#2e5a4c" },
+    NvimTreeFolderIcon = { fg = "#2e5a4c" },
+    NvimTreeEmptyFolderName = { fg = "#2e5a4c" },
+    NvimTreeRootFolder = { fg = "#1a6b5a", bold = true },
+    NvimTreeIndentMarker = { fg = "#c2ddc8" },
+    -- Tabline background follows theme
+    TbFill = { bg = "black2" },
+    TbBufOn = { fg = "white", bg = "black" },
+    TbBufOff = { fg = "light_grey", bg = "black2" },
+    TbBufOnClose = { fg = "red", bg = "black" },
+    TbBufOffClose = { fg = "grey_fg", bg = "black2" },
+    TbBufOnModified = { fg = "green", bg = "black" },
+    TbBufOffModified = { fg = "red", bg = "black2" },
+    TbTabOn = { fg = "red", bg = "black" },
+    TbTabOff = { fg = "light_grey", bg = "black2" },
+    TbTabNewBtn = { fg = "light_grey", bg = "one_bg" },
+    TbThemeToggleBtn = { fg = "light_grey", bg = "one_bg2" },
+    TbCloseAllBufsBtn = { fg = "black", bg = "red" },
+    -- Markdown / treesitter: fix dark backgrounds leaking in light mode
+    ["@markup.quote"] = { bg = "black2" },
+    ["@markup.raw"] = { bg = "black2" },
+  },
+}
+return M
---
+++ lua/plugins/init-2.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "Mooling0602/base46",
+    branch = "v3.0",
+    lazy = true,
+  },
+}
